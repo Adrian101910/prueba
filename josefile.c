3f2f863b8906c822d458b0cd9757b5aecc4389f0
Kingm4s <139110084+Kingm4s@users.noreply.github.com>
2024-07-30 03:28:55 +0200

    Update josefile.c

--- josefile.c
+++ josefile.c
@@ -1,32 +1,37 @@
 #include <unistd.h>
 
-int main(void)
+// Función para imprimir todas las combinaciones de tres dígitos en el rango 000-999
+void ft_print_comb3(void)
 {
-    char n1;
+    char n1 = '0';
     char n2;
     char n3;
 
-    n1 = '0';
-    n2 = '0';
-    n3 = '0';
-
     while (n1 <= '9')
     {
+        n2 = n1; // Inicializar n2 con n1
         while (n2 <= '9')
         {
-            while(n3 <= '9')
+            n3 = n2; // Inicializar n3 con n2
+            while (n3 <= '9')
             {
-                write(1, &n1, 1);
-                write(1, &n2, 1);
-                write(1, &n3, 1);
-                write(1, "\n", 1);
-                ++n3;
-            }    
-            n2++;
-            n3 = n2 + 1;
+                write(1, &n1, 1);  // Imprimir primer dígito
+                write(1, &n2, 1);  // Imprimir segundo dígito
+                write(1, &n3, 1);  // Imprimir tercer dígito
+                if (!(n1 == '9' && n2 == '9' && n3 == '9'))
+                {
+                    write(1, ", ", 2);  // Imprimir coma y espacio si no es la última combinación
+                }
+                n3++;  // Incrementar el tercer dígito
+            }
+            n2++;  // Incrementar el segundo dígito
         }
-        n2 = n1 + 1;
-        n1++;
-    }     
+        n1++;  // Incrementar el primer dígito
+    }
+}
+
+int main(void)
+{
+    ft_print_comb3();
     return 0;
 }
